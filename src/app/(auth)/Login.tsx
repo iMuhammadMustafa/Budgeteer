82341239593735c799dd831595851c731775d7f2
Muhammad Mustafa <Muhammad.M@miviewis.com>
2024-07-30 10:26:39 -0500

    Update Login.tsx

--- src/app/(auth)/Login.tsx
+++ src/app/(auth)/Login.tsx
@@ -1,7 +1,8 @@
 import { supabase } from "@/src/lib/supabase";
 import { User } from "@/src/data/models/User";
 import React, { useEffect, useState } from "react";
-import { Alert, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Alert, SafeAreaView, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { router } from "expo-router";
 
 export default function Login() {
   const [loading, setLoading] = useState(false);
@@ -22,19 +23,19 @@ export default function Login() {
   }
 
   return (
-    <View className="flex-1 justify-center items-center p-4">
-      <Text className="text-2xl font-bold mb-6">Login</Text>
-      <TextInput placeholder="Username" className=" p-4 mb-4 border border-teal-900 rounded-lg text-lg text-teal-900" />
-      <TextInput placeholder="Password" secureTextEntry className=" p-4 mb-4 border  rounded-lg text-lg" />
-      <TouchableOpacity className=" p-4 mb-4 bg-primary dark:bg-white rounded-lg items-center">
-        <Text className=" text-white">Login</Text>
+    <SafeAreaView className="flex-col justify-center p-4 h-full w-full md:w-[50%]">
+    <Text className="text-foreground text-2xl font-bold mb-10 text-center">Login</Text>
+      <TextInput placeholder="Username" className="p-4 mb-4 border border-primary rounded-lg text-lg bg-white text-foreground " onChangeText={text => setUser({ ...user, email: text })} />
+      <TextInput placeholder="Password" secureTextEntry className=" p-4 mb-4 border border-primary rounded-lg text-lg bg-white text-foreground" onChangeText={text => setUser({ ...user, password: text })}/>
+      <TouchableOpacity className="p-4 mb-4 bg-primary rounded-lg items-center" onPress= {signInWithEmail} disabled={loading}>
+        <Text className="text-foreground">Login</Text>
       </TouchableOpacity>
-      <TouchableOpacity className=" p-4 mb-4 bg-gray-500 rounded-lg items-center">
-        <Text className="text-white">Register</Text>
+      <TouchableOpacity className=" p-4 mb-4 bg-secondary rounded-lg items-center" onPress={() =>router.push("/Register")}>
+        <Text className="text-foreground">Register</Text>
       </TouchableOpacity>
       <TouchableOpacity>
         <Text className="text-blue-500">Forgot Password?</Text>
       </TouchableOpacity>
-    </View>
+    </SafeAreaView>
   );
 }
