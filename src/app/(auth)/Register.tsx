c0333bb83553c6e837f2b955a961c4178e7dca28
Muhammad Mustafa <Muhammad.M@miviewis.com>
2024-07-30 10:26:40 -0500

    Update Register.tsx

--- src/app/(auth)/Register.tsx
+++ src/app/(auth)/Register.tsx
@@ -1,6 +1,6 @@
 import { supabase } from "@/src/lib/supabase";
 import { useState } from "react";
-import { Alert, Button, TextInput, View } from "react-native";
+import { Alert, Button, SafeAreaView, TextInput, View } from "react-native";
 
 export default function Register() {
   const [loading, setLoading] = useState(false);
@@ -18,11 +18,11 @@ export default function Register() {
   }
 
   return (
-    <View>
+    <SafeAreaView className="h-full w-full md:w-auto">
       <TextInput placeholder="Email" onChangeText={text => setUser({ ...user, email: text })} />
       <TextInput placeholder="Password" onChangeText={text => setUser({ ...user, password: text })} />
       <TextInput placeholder="Confirm Password" onChangeText={text => setUser({ ...user, confirmPassword: text })} />
       <Button title="Sign Up" onPress={signUpWithEmail} disabled={loading} />
-    </View>
+    </SafeAreaView>
   );
 }
