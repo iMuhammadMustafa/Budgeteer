f40e52e8a197394c2ca2999692791ed6bc48761f
Muhammad Mustafa <Muhammad.M@miviewis.com>
2024-07-31 15:39:06 -0500

    Styling: Style Register Page

--- src/app/(auth)/Register.tsx
+++ src/app/(auth)/Register.tsx
@@ -1,15 +1,17 @@
+import { User } from "@/src/data/models/User";
 import { supabase } from "@/src/lib/supabase";
 import { useState } from "react";
-import { Alert, Button, SafeAreaView, TextInput, View } from "react-native";
+import { Alert, TouchableOpacity, SafeAreaView, TextInput, View, Text } from "react-native";
+import { router } from "expo-router";
 
 export default function Register() {
   const [loading, setLoading] = useState(false);
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState<User>();
 
   async function signUpWithEmail() {
     setLoading(true);
     const { error } = await supabase.auth.signUp({
-      email: user.email,
+      email: user?.email,
       password: user.password,
     });
 
@@ -18,11 +20,17 @@ export default function Register() {
   }
 
   return (
-    <SafeAreaView className="h-full w-full md:w-auto">
-      <TextInput placeholder="Email" onChangeText={text => setUser({ ...user, email: text })} />
-      <TextInput placeholder="Password" onChangeText={text => setUser({ ...user, password: text })} />
-      <TextInput placeholder="Confirm Password" onChangeText={text => setUser({ ...user, confirmPassword: text })} />
-      <Button title="Sign Up" onPress={signUpWithEmail} disabled={loading} />
+    <SafeAreaView className="flex-col justify-center m-auto p-4 h-full w-full md:w-[50%]">
+      <Text className="text-foreground text-2xl font-bold mb-10 text-center">Register</Text>
+      <TextInput className="border rounded-lg bg-white my-2 p-4 text-lg" placeholder="Email" onChangeText={text => setUser({ ...user, email: text })} />
+      <TextInput className="border rounded-lg bg-white my-2 p-4 text-lg" placeholder="Password" onChangeText={text => setUser({ ...user, password: text })} />
+      <TextInput className="border rounded-lg bg-white my-2 p-4 text-lg" placeholder="Confirm Password" onChangeText={text => setUser({ ...user, confirmPassword: text })} />
+      <TouchableOpacity className="py-4 my-2 mb-4 bg-primary rounded-lg items-center" onPress= {signUpWithEmail} disabled={loading}>
+        <Text className="text-foreground">Register</Text>
+      </TouchableOpacity>
+      <TouchableOpacity className=" py-4 mb-4 bg-secondary rounded-lg items-center" onPress={() =>router.push("/Login")}>
+        <Text className="text-foreground">Login</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 }
