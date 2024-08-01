d4b8d8f74116de834ab0857e41c943a20fc2e6fc
Muhammad Mustafa <Muhammad.M@miviewis.com>
2024-08-01 15:53:36 -0500

    Add Mock File for data

--- src/data/entities/Index.ts
+++ src/data/entities/Index.ts
@@ -3,3 +3,6 @@ import { BaseEntity } from "./BaseEntity";
 import { UserAccount } from "./Account";
 import { Transaction } from "./Transaction";
 import { Category } from "./Category";
+
+
+export { Account, BaseEntity, UserAccount, Transaction, Category };
