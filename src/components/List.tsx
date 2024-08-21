76adaafdf0951da98eac16408a4514004aee4cdc
Muhammad Mustafa <Muhammad.M@miviewis.com>
2024-08-21 08:36:24 -0500

    Fix List width and TS errors

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,5 +1,5 @@
 import { Box } from "@/components/ui/box";
-import { Link } from "expo-router";
+import { Href, Link } from "expo-router";
 import Icon from "../lib/IonIcons";
 import { Table, TableHeader, TableRow, TableHead, TableBody, TableData } from "@/components/ui/table";
 import { ScrollView, TouchableOpacity } from "react-native";
@@ -7,21 +7,21 @@ import { ScrollView, TouchableOpacity } from "react-native";
 export interface ListProps<T> {
   data: T[];
   columns: string[];
-  createLink: string;
+  createLink: Href<string>;
   renderItem: (item: T) => React.ReactNode;
 }
 
 export default function List<T>({ data, columns, createLink, renderItem }: ListProps<T>) {
   return (
-    <ScrollView horizontal>
-      <Box className="my-4 mx-5 flex">
-        <Box className="self-end my-2">
-          <Link href={createLink}>
-            <Icon name="Plus" className="text-foreground" />
-          </Link>
-        </Box>
-        <Box className="border border-solid border-outline-200 rounded-lg overflow-scroll">
-          <Table className="">
+    <Box className="my-4 mx-5 flex">
+      <Box className="self-end my-2">
+        <Link href={createLink}>
+          <Icon name="Plus" className="text-foreground" />
+        </Link>
+      </Box>
+      <Box className="border border-solid border-outline-200 rounded-lg overflow-scroll">
+        <ScrollView horizontal contentContainerClassName="w-full">
+          <Table className="w-full">
             <TableHeader>
               <TableRow className="border-b border-outline-200">
                 {columns.map(coloumn => (
@@ -32,12 +32,12 @@ export default function List<T>({ data, columns, createLink, renderItem }: ListP
             <TableBody>
               {data &&
                 data.map((item: T) => {
-                  return <>{renderItem(item)}</>;
+                  return renderItem(item);
                 })}
             </TableBody>
           </Table>
-        </Box>
+        </ScrollView>
       </Box>
-    </ScrollView>
+    </Box>
   );
 }
