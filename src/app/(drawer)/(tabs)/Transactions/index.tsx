b26e74157dcd6530ab4464f270cc0c971b4cf69a
iMuhammadMustafa <iMuhammadMustafa@gmail.com>
2024-09-11 18:21:54 -0500

    Add Copy and Patch Delete

--- src/app/(drawer)/(tabs)/Transactions/index.tsx
+++ src/app/(drawer)/(tabs)/Transactions/index.tsx
@@ -2,8 +2,12 @@ import { Box } from "@/components/ui/box";
 import { TableRow, TableData } from "@/components/ui/table";
 import List from "@/src/components/List";
 import Icon from "@/src/lib/IonIcons";
-import { Transaction } from "@/src/lib/supabase";
-import { useDeleteTransaction, useGetTransactions } from "@/src/repositories/transactions.service";
+import { Inserts, Transaction } from "@/src/lib/supabase";
+import {
+  useDeleteTransaction,
+  useGetTransactions,
+  useUpsertTransaction,
+} from "@/src/repositories/transactions.service";
 import { Link, useRouter } from "expo-router"; // Use `useRouter` for navigation
 import React, { useState } from "react";
 import dayjs from "dayjs";
@@ -12,16 +16,19 @@ import relativeTime from "dayjs/plugin/relativeTime";
 import { View, Text, FlatList, ScrollView, SafeAreaView, ActivityIndicator, Pressable } from "react-native";
 import { useAuth } from "@/src/providers/AuthProvider";
 import { useNotifications } from "@/src/providers/NotificationsProvider";
+import { TableNames } from "@/src/consts/TableNames";
+import { TransactionFormType } from "@/src/components/pages/TransactionFormNew";
 
 export default function Transactions() {
   const { data: transactions, error, isLoading } = useGetTransactions();
   const [isActionLoading, setIsActionLoading] = useState(false);
-  const [selectedTransactions, setSelectedTransactions] = useState<string[]>([]);
+  const [selectedTransactions, setSelectedTransactions] = useState<TransactionFormType[]>([]);
   const [selectedSum, setSelectedSum] = useState(0);
   const [selectionMode, setSelectionMode] = useState(false); // To track if we're in selection mode
   const mutation = useDeleteTransaction();
   const router = useRouter(); // Expo Router hook for navigation
   const { addNotification } = useNotifications();
+  const addMutation = useUpsertTransaction();
 
   const deleteMutation = useDeleteTransaction();
 
@@ -47,27 +54,26 @@ export default function Transactions() {
   dayjs.extend(relativeTime);
 
   // Long press to start selection mode and select the first transaction
-  const handleLongPress = (id: string) => {
+  const handleLongPress = (item: any) => {
     setSelectionMode(true);
-    setSelectedTransactions(prev => (prev.includes(id) ? prev.filter(t => t !== id) : [...prev, id]));
-    setSelectedSum(prev => prev + (transactions.find(t => t.id === id)?.amount ?? 0));
+    setSelectedTransactions(prev => [...prev, item]);
+    setSelectedSum(prev => prev + item.amount);
   };
 
   // Short press behavior depends on selection mode
-  const handlePress = (id: string) => {
+  const handlePress = (item: any) => {
     if (selectionMode) {
       // In selection mode, short press selects/deselects
-      const updatedSelections = selectedTransactions.includes(id)
-        ? selectedTransactions.filter(t => t !== id)
-        : [...selectedTransactions, id];
+      const updatedSelections = selectedTransactions.find(i => i.id === item.id)
+        ? selectedTransactions.filter(t => t.id !== item.id)
+        : [...selectedTransactions, item];
 
       setSelectedTransactions(updatedSelections);
 
       // Update selected sum
       setSelectedSum(
         updatedSelections.reduce((acc, curr) => {
-          const transaction = transactions.find(t => t.id === curr);
-          return acc + (transaction?.amount ?? 0);
+          return acc + (curr?.amount ?? 0);
         }, 0),
       );
 
@@ -78,7 +84,7 @@ export default function Transactions() {
     } else {
       // Outside selection mode, navigate to transaction details
       // Use router for navigation
-      router.push(`../AddTransaction?transactionId=${id}`);
+      router.push(`../AddTransaction?transactionId=${item.id}`);
     }
   };
 
@@ -88,56 +94,90 @@ export default function Transactions() {
     setSelectionMode(false); // Clear selection mode when we clear selections
   };
 
+  const copyTransactions = async () => {
+    setIsActionLoading(true);
+
+    try {
+      for (let item of selectedTransactions) {
+        const { account, category, ...newTransaction } = { ...item };
+
+        await addMutation.mutateAsync(
+          {
+            fullFormTransaction: {
+              ...newTransaction,
+              id: null,
+              date: dayjs().toISOString(),
+              createdat: dayjs().toISOString(),
+              updatedat: null,
+              createdby: null,
+              updatedby: null,
+            },
+          },
+          {
+            onSuccess: () => addNotification({ message: "Transaction Created Successfully", type: "success" }),
+          },
+        );
+      }
+    } catch (error) {
+      addNotification({ message: "Error creating transactions", type: "error" });
+    } finally {
+      setIsActionLoading(false);
+      clearSelection();
+    }
+  };
+
   const deleteSelection = async () => {
     setIsActionLoading(true);
 
     // Delete selected transactions
-    await Promise.all(
-      selectedTransactions.map(id =>
-        deleteMutation.mutateAsync(
-          transactions.find(t => t.id === id),
-          {
-            onSuccess: () => {
-              addNotification({ message: "Transaction Created Successfully", type: "success" });
-              setIsActionLoading(false);
-            },
-          },
-        ),
-      ),
-    );
+    // await Promise.all(
+    for (let item of selectedTransactions) {
+      await deleteMutation.mutateAsync(item, {
+        onSuccess: () => {
+          addNotification({ message: "Transaction Deleted Successfully", type: "success" });
+          setIsActionLoading(false);
+        },
+      });
+    }
+
+    // );
 
     // Clear selections and exit selection
 
     clearSelection();
   };
 
-  const renderTransaction = transaction => (
-    <Pressable
-      onLongPress={() => handleLongPress(transaction.id)}
-      onPress={() => handlePress(transaction.id)} // Handles selection or navigation based on selectionMode
-      className={`m-3 flex-row items-center justify-between gap-7 flex-1 ${selectedTransactions.includes(transaction.id) ? "bg-blue-200" : "bg-white"}`} // Highlighting selected items
-    >
-      <View className="rounded-full h-10 w-10 bg-neutral-100 flex justify-center items-center">
-        <TransactionTypeIcon transaction={transaction} />
-      </View>
-      <View className="flex-1">
-        <Text>{transaction.description ?? transaction.category?.name ?? "Hello"}</Text>
-        <View className="flex-row justify-start items-center gap-2">
-          <Text>{transaction.category?.name}</Text>
+  const renderTransaction = transaction => {
+    return (
+      <Pressable
+        onLongPress={() => handleLongPress(transaction)}
+        onPress={() => handlePress(transaction)} // Handles selection or navigation based on selectionMode
+        className={`m-3 flex-row items-center justify-between gap-7 flex-1 ${selectedTransactions.find(t => t.id === transaction.id) ? "bg-blue-200" : "bg-white"}`} // Highlighting selected items
+      >
+        <View className="rounded-full h-10 w-10 bg-neutral-100 flex justify-center items-center">
+          <TransactionTypeIcon transaction={transaction} />
         </View>
-      </View>
-      <View className="flex items-end">
-        <Text className={`${getTransactionProp(transaction.type).color}`}>
-          {transaction.amount.toLocaleString(undefined, {
-            minimumFractionDigits: 2,
-            maximumFractionDigits: 2,
-          })}
-          {transaction.account?.currency}
-        </Text>
-        <Text>{transaction.account?.name}</Text>
-      </View>
-    </Pressable>
-  );
+        <View className="flex-1">
+          <Text>{transaction.description ?? transaction.category?.name ?? "Hello"}</Text>
+          <View className="flex-row justify-start items-center gap-2">
+            <Text>{transaction.category?.name}</Text>
+          </View>
+        </View>
+        <View className="flex items-end">
+          <Text className={`${getTransactionProp(transaction.type).color}`}>
+            {transaction.amount.toLocaleString(undefined, {
+              minimumFractionDigits: 2,
+              maximumFractionDigits: 2,
+            })}
+            {transaction.account?.currency}
+          </Text>
+          <Text>
+            {transaction.account?.name} {transaction.account?.balance}
+          </Text>
+        </View>
+      </Pressable>
+    );
+  };
 
   return (
     <SafeAreaView className="w-full h-full">
@@ -149,6 +189,9 @@ export default function Transactions() {
             <Text className="text-lg text-primary-500 mr-4">{selectedSum} USD</Text>
             <Pressable onPress={deleteSelection}>
               <Icon name="Trash" className="text-error-500" />
+            </Pressable>
+            <Pressable onPress={copyTransactions}>
+              <Icon name="Copy" className="text-info-500" />
             </Pressable>
             <Pressable onPress={clearSelection}>
               <Icon name="X" className="text-danger-500" />
