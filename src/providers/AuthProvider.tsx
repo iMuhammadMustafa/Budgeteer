6d70982653ec2fd95070c09dbe3577f4d1cd22b7
iMuhammadMustafa <29302054+iMuhammadMustafa@users.noreply.github.com>
2025-02-02 15:33:52 -0600

    Add Auth Provider

---
+++ src/providers/AuthProvider.tsx
@@ -0,0 +1,40 @@
+import { PropsWithChildren, createContext, useContext, useEffect, useState } from "react";
+import { Session } from "@supabase/supabase-js";
+import supabase from "./Supabase";
+
+type AuthType = {
+  session: Session | null;
+  user?: Session["user"];
+  isSessionLoading: boolean;
+};
+
+const AuthContext = createContext<AuthType>({
+  session: null,
+  isSessionLoading: true,
+});
+
+export default function AuthProvider({ children }: PropsWithChildren) {
+  const [session, setSession] = useState<Session | null>(null);
+  const user = session?.user;
+  const [isSessionLoading, setIsSessionLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchSession = async () => {
+      const { data, error } = await supabase.auth.getSession();
+      setSession(data.session);
+      setIsSessionLoading(false);
+    };
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+    fetchSession();
+  }, []);
+
+  return <AuthContext.Provider value={{ session, user, isSessionLoading }}>{children}</AuthContext.Provider>;
+}
+// export const useAuth = () => useContext(AuthContext);
+export const useAuth = () => {
+  const authContext = useContext(AuthContext);
+  if (!authContext) throw new Error("useAuth must be used within an AuthProvider");
+  return authContext;
+};
--- src/providers/Supabase.ts
+++ src/providers/Supabase.ts
@@ -6,15 +6,16 @@ import { Database } from "@/src/types/database.types";
 // TODO: Replace with your own Supabase URL and Anon Key
 // const supaUrl = process.env.EXPO_PUBLIC_SUPA_URL as string;
 // const supaKey = process.env.EXPO_PUBLIC_SUPA_ANON_KEY as string;
-const supabase = createClient<Database>(
-  "https://eqevunpprealeqzqpgbc.supabase.co",
-  "qEW8Y9XZCmGifGZQMsu1v5Uhf2N8+rM1iIlL/yuwNjxToZaJ4ht+JEqx3L6ha/5loyF5rJxtBDeP9Xs9iosvBA==",
-  {
-    auth: {
-      storage: AsyncStorage,
-      autoRefreshToken: true,
-      persistSession: true,
-      detectSessionInUrl: false,
-    },
+const supabaseUrl = "https://eqevunpprealeqzqpgbc.supabase.co";
+const supabaseAnonKey = "qEW8Y9XZCmGifGZQMsu1v5Uhf2N8+rM1iIlL/yuwNjxToZaJ4ht+JEqx3L6ha/5loyF5rJxtBDeP9Xs9iosvBA==";
+
+const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
   },
-);
+});
+
+export default supabase;
