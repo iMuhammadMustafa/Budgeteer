945985768eaf2118130c8761771ef42b2b38fa8a
iMuhammadMustafa <29302054+iMuhammadMustafa@users.noreply.github.com>
2025-02-03 18:13:31 -0600

    Update 20250201103718_create_tables.sql

--- supabase/migrations/20250201103718_create_tables.sql
+++ supabase/migrations/20250201103718_create_tables.sql
@@ -38,7 +38,7 @@ create table AccountCategories
 );
 
 Alter table AccountCategories Enable Row Level Security; 
-CREATE POLICY "Tenant access" ON AccountCategories FOR SELECT USING (TenantId = auth.tenantid());
+CREATE POLICY "Tenant access" ON AccountCategories as PERMISSIVE for ALL USING (TenantId = auth.tenantid());
 CREATE INDEX "AccountCategories_TenantId" ON AccountCategories (TenantId);
 CREATE INDEX "AccountCategories_IsDeleted" ON AccountCategories (IsDeleted);
 -- CREATE POLICY "Tenant insert" ON AccountCategories FOR INSERT WITH CHECK (TenantId = auth.jwt()->>'tenantid');
@@ -73,7 +73,7 @@ create table Accounts
 );
 
 Alter table Accounts Enable Row Level Security;
-CREATE POLICY "Tenant access" ON Accounts FOR SELECT USING (TenantId = auth.tenantid());
+CREATE POLICY "Tenant access" ON Accounts as PERMISSIVE  for ALL USING (TenantId = auth.tenantid());
 CREATE INDEX "Accounts_TenantId" ON Accounts (TenantId);
 CREATE INDEX "Accounts_IsDeleted" ON Accounts (IsDeleted);
 
@@ -127,7 +127,7 @@ CREATE TABLE TransactionGroups
 );
 
 Alter table TransactionGroups Enable Row Level Security;
-CREATE POLICY "Tenant access" ON TransactionGroups FOR SELECT USING (TenantId = auth.tenantid());
+CREATE POLICY "Tenant access" ON TransactionGroups as PERMISSIVE for ALL USING (TenantId = auth.tenantid());
 CREATE INDEX "TransactionGroups_TenantId" ON TransactionGroups (TenantId);
 CREATE INDEX "TransactionGroups_IsDeleted" ON TransactionGroups (IsDeleted);
 -- CREATE TRIGGER SetDefaultValues BEFORE INSERT ON TransactionGroups FOR EACH ROW EXECUTE FUNCTION SetDefaultValues();
@@ -160,7 +160,7 @@ CREATE TABLE TransactionCategories(
 );
 
 Alter table TransactionCategories Enable Row Level Security;
-CREATE POLICY "Tenant access" ON TransactionCategories FOR SELECT USING (TenantId = auth.tenantid());
+CREATE POLICY "Tenant access" ON TransactionCategories as PERMISSIVE for ALL USING (TenantId = auth.tenantid());
 CREATE INDEX "TransactionCategories_TenantId" ON TransactionCategories (TenantId);
 CREATE INDEX "TransactionCategories_IsDeleted" ON TransactionCategories (IsDeleted);
 -- CREATE TRIGGER SetDefaultValues BEFORE INSERT ON TransactionCategories FOR EACH ROW EXECUTE FUNCTION SetDefaultValues();
@@ -205,7 +205,7 @@ CREATE TABLE Transactions
 );
 
 Alter table Transactions Enable Row Level Security;
-CREATE POLICY "Tenant access" ON Transactions FOR SELECT USING (TenantId = auth.tenantid());
+CREATE POLICY "Tenant access" ON Transactions as PERMISSIVE  for ALL USING (TenantId = auth.tenantid());
 CREATE INDEX "Transactions_TenantId" ON Transactions (TenantId);
 CREATE INDEX "Transactions_IsDeleted" ON Transactions (IsDeleted);
 CREATE INDEX "Transactions_Accountid_Date" ON transactions (accountid, date, id) WHERE IsDeleted = false AND IsVoid = false;;
