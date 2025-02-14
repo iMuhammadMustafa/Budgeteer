4519887b98d9cdb1ab5dc126a9c821a75b3e04d5
MuhammadMustafa-IS <Muhammad.M@miviewis.com>
2025-02-14 17:20:31 -0600

    fix routes

--- src/app/(drawer)/(tabs)/Dashboard/_layout.tsx
+++ src/app/(drawer)/(tabs)/Dashboard/_layout.tsx
@@ -3,7 +3,7 @@ import { Stack } from "expo-router";
 export default function TransactionsLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Dashboard" />
+      <Stack.Screen name="index" />
     </Stack>
   );
 }
