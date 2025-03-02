99bd5804d4b41ab512aa3f8f593ee74e9e925a7d
iMuhammadMustafa <29302054+iMuhammadMustafa@users.noreply.github.com>
2025-03-02 09:40:12 -0600

    Create MyModal.tsx

---
+++ src/components/MyModal.tsx
@@ -0,0 +1,31 @@
+import { Modal, Pressable, ScrollView } from "react-native";
+
+export default function MyModal({ isOpen, setIsOpen, children }: any) {
+  return (
+    <Modal
+      visible={isOpen}
+      onDismiss={() => setIsOpen(false)}
+      transparent={true}
+      animationType="fade"
+      className="flex-1 justify-center items-center"
+      // presentationStyle="pageSheet"
+      // onBackButtonPress={() => setIsOpen(false)}
+      // onRequestClose={() => setIsOpen(false)}
+      // onBackdropPress={() => setIsOpen(false)}
+      // className="rounded-md z-50 bg-white"
+      // transparent
+      // presentationClassName="bg-transparent"
+    >
+      <Pressable
+        onPressOut={() => setIsOpen(false)}
+        className="bg-black bg-opacity-50 flex-1 justify-center items-center cursor-auto"
+      >
+        <Pressable className="flex-1 " onPress={e => e.stopPropagation()}>
+          <ScrollView className="p-4 rounded-md border border-muted flex-grow-0 m-auto bg-card cursor-auto">
+            {children}
+          </ScrollView>
+        </Pressable>
+      </Pressable>
+    </Modal>
+  );
+}
