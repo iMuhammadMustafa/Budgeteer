b69b099067be2b4518ae92d539a7764664f32969
iMuhammadMustafa <29302054+iMuhammadMustafa@users.noreply.github.com>
2025-03-02 09:40:37 -0600

    Fix Search UI

--- src/components/pages/Transactions/SearchModal.tsx
+++ src/components/pages/Transactions/SearchModal.tsx
@@ -1,9 +1,9 @@
 import { useEffect } from "react";
-import { BackHandler, Platform, View, Modal } from "react-native";
+import { BackHandler, Platform, View, Modal, Pressable } from "react-native";
 
 import { TransactionSearchFormProps } from "@/src/types/pages/Transactions.types";
 import TransactionSearchForm from "./SearchForm";
-import { Pressable } from "react-native-gesture-handler";
+import MyModal from "../../MyModal";
 
 export default function TransactionSearchModal({
   isOpen,
@@ -45,36 +45,17 @@ export default function TransactionSearchModal({
   }, [isOpen]);
 
   return isOpen ? (
-    <Modal
-      visible={isOpen}
-      onDismiss={() => setIsOpen(false)}
-      transparent={true}
-      // presentationStyle="pageSheet"
-      // onBackButtonPress={() => setIsOpen(false)}
-      // onRequestClose={() => setIsOpen(false)}
-      // onBackdropPress={() => setIsOpen(false)}
-      // className="rounded-md z-50 bg-white"
-      // transparent
-      // presentationClassName="bg-transparent"
-    >
-      <Pressable
-        className="bg-black bg-opacity-50 flex-1 justify-center items-center"
-        onPressOut={() => setIsOpen(false)}
-        onPress={() => {}}
-      >
-        <View className="m-auto p-4 rounded-md border border-muted flex-grow-0 max-w-xs overflow-x-hidden bg-card">
-          <TransactionSearchForm
-            filters={searchParams}
-            accounts={accounts}
-            categories={categories}
-            onSubmit={filters => {
-              onSubmit(filters);
-            }}
-            onClear={onClear}
-          />
-        </View>
-      </Pressable>
-    </Modal>
+    <MyModal isOpen={isOpen} setIsOpen={setIsOpen}>
+      <TransactionSearchForm
+        filters={searchParams}
+        accounts={accounts}
+        categories={categories}
+        onSubmit={filters => {
+          onSubmit(filters);
+        }}
+        onClear={onClear}
+      />
+    </MyModal>
   ) : (
     <></>
   );
