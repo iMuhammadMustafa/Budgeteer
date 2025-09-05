2e27ace347483d34ec5dfdd4e99056ab6d65dabb
iMuhammadMustafa <29302054+iMuhammadMustafa@users.noreply.github.com>
2025-09-05 17:52:15 -0500

    Update index.tsx

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,19 +1,13 @@
-import { router } from "expo-router";
-import { ActivityIndicator, SafeAreaView, ScrollView, View } from "react-native";
-import { useAuth } from "@/src/providers/AuthProvider";
+import { SafeAreaView, ScrollView, View } from "react-native";
 
 import BudgeteerLanding from "@/src/components/pages/VLanding";
 
 export default function Index() {
-  const { session, isSessionLoading } = useAuth();
-
-  if (isSessionLoading) return <ActivityIndicator />;
-
   return (
     <SafeAreaView className="flex-1 bg-background-100">
       <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
         <View className="flex-1 justify-center items-center">
-          <BudgeteerLanding session={session} router={router} />
+          <BudgeteerLanding />
         </View>
       </ScrollView>
     </SafeAreaView>
