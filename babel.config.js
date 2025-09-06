8429d439459d66123d2efbe63a8dc60a3b52b476
iMuhammadMustafa <29302054+iMuhammadMustafa@users.noreply.github.com>
2025-09-05 21:06:09 -0500

    Install Packages and Add dotfiles

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
+      "module:metro-react-native-babel-preset",
+    ],
+    plugins: [
+      ["@babel/plugin-proposal-decorators", { legacy: true }],
+      ["@babel/plugin-proposal-class-properties", { loose: true }],
+      ["@babel/plugin-transform-runtime", { helpers: true, regenerator: true }],
+    ],
+  };
+};
