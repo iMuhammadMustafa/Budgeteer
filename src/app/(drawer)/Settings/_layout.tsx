ab31c5110aaa6d42c4602ef2e7ab3b6547f8d1d1
iMuhammadMustafa <29302054+iMuhammadMustafa@users.noreply.github.com>
2025-09-05 20:34:10 -0500

    Clean Everything

--- src/app/(drawer)/Settings/_layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Stack } from "expo-router";
-
-export default function SettingsStack() {
-  return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-    </Stack>
-  );
-}
