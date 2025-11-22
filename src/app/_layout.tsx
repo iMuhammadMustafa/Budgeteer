12af0f6f93fc8e7d0effdc733cd1f898b9451604
iMuhammadMustafa <29302054+iMuhammadMustafa@users.noreply.github.com>
2025-11-21 19:26:14 -0600

    feat: Add relativeTime plugin to dayjs for improved date formatting

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -7,6 +7,7 @@ import AuthProvider from "../providers/AuthProvider";
 import StorageModeProvider from "../providers/StorageModeProvider";
 
 import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
 import timezone from "dayjs/plugin/timezone";
 import utc from "dayjs/plugin/utc";
 import { Suspense } from "react";
@@ -15,6 +16,7 @@ import QueryProvider from "../providers/QueryProvider";
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
+dayjs.extend(relativeTime);
 
 export default function RootLayout() {
   return (
